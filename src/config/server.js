ff8a5f76cf6d87e22ee1835cce0a8026411c610d
Ramon Velasco <ramonmaito@gmail.com>
2020-09-04 08:18:38 -0300

    Ajustando backend

--- src/config/server.js
+++ src/config/server.js
@@ -7,9 +7,10 @@ const allowCors = require('./cors')
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function() {
-console.log(`BACKED is running on port ${port}.`)
+    console.log(`BACKEND is running on port ${port}.`)
 })
 
 module.exports = server
